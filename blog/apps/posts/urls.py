0683260b2a998873cc633e9deea09b9d43dd1fb1
Lucia Misiaszek <lumisiaszek@gmail.com>
2024-10-10 19:26:29 -0300

    Noticia individual: html, urls, views / comentarios en proceso

--- blog/apps/posts/urls.py
+++ blog/apps/posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import posts, quienessomos
+from .views import posts, quienessomos, postindividual
 
 urlpatterns = [
     path ('', posts, name = 'posts'),
     path('quienessomos/', quienessomos, name='quienessomos'), 
+    path('post/<int:post_id>/', postindividual, name='postindividual'),
 ]
 
 
--- blog/apps/posts/views.py
+++ blog/apps/posts/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from .models import Post, Categoria
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Categoria, Comentario
 from django.core.paginator import Paginator
+from .form import ComentarioForm
+
 
 def posts(request):
     categorias = Categoria.objects.all()  
@@ -19,6 +21,32 @@ def posts(request):
 
     return render(request, 'index.html', {'posts': page_obj, 'categorias': categorias})
 
+
+#vista para quienes somos
 def quienessomos(request):
     return render(request, 'quienessomos.html')
+
+
+# Vista para mostrar un post específico y manejar comentarios
+def postindividual(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    comentarios = Comentario.objects.filter(post=post).order_by('-fecha_publicacion')  
+
+    if request.method == 'POST':
+        form = ComentarioForm(request.POST)
+        if form.is_valid():
+            comentario = form.save(commit=False)
+            comentario.post = post
+            comentario.autor = request.user  
+            comentario.save()
+            return redirect('postindividual', post_id=post.id) 
+    else:
+        form = ComentarioForm()
+
+    return render(request, 'postindividual.html', {
+        'post': post,
+        'comentarios': comentarios,
+        'form': form
+    })
+
 
