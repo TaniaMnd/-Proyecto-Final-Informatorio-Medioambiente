1d985f8592b9ddeee7e73d7b0298cabed3208e15
Lucia Misiaszek <lumisiaszek@gmail.com>
2024-10-10 16:26:48 -0300

    Actualizacion de view.posts, index y html

--- blog/apps/posts/views.py
+++ blog/apps/posts/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Categoria
+from django.core.paginator import Paginator
 
 def posts(request):
-  posts = Post.objects.all()
-  print(posts)
-  return render(request, 'index.html', {'posts' : posts})
+    categorias = Categoria.objects.all()  
+    selected_categoria = request.GET.get('categoria')  
+
+    # Filtrar los posts
+    if selected_categoria:
+        posts = Post.objects.filter(categoria__nombre=selected_categoria)  
+    else:
+        posts = Post.objects.all()  
+
+    # Paginación
+    paginator = Paginator(posts, 9)  
+    page_number = request.GET.get('page')  
+    page_obj = paginator.get_page(page_number) 
+
+    return render(request, 'index.html', {'posts': page_obj, 'categorias': categorias})
 
