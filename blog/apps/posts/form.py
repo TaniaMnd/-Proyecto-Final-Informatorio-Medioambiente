7a634afe8edbd49c8c8f6b304c6e3d54d716e15e
Lumisiaszek <lumisiaszek@gmail.com>
2024-10-11 19:56:11 -0300

    Comentarios en noticia particular / estructura y estilo de noticia particular y comentarios (en proceso)

--- blog/apps/posts/form.py
+++ blog/apps/posts/form.py
@@ -5,3 +5,11 @@ class ComentarioForm(forms.ModelForm):
     class Meta:
         model = Comentario
         fields = ['contenido'] 
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['contenido'].widget.attrs.update({
+            'placeholder': 'Escribe tu comentario..',
+            'rows': '1',
+            'class': 'form-control'  
+        })
