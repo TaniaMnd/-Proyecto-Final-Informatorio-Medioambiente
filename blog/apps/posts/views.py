e73cb87d2a9e6fc660f88e004b448a756dfe282a
Cecilia <cecilia@MacBook-Air-de-Cecilia.local>
2024-10-15 02:16:30 -0300

    mapeo urls + user Register, funcionalidades básicas+login/logout +templates

--- blog/apps/posts/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Post
-
-def posts(request):
-  posts = Post.objects.all()
-  print(posts)
-  return render(request, 'index.html', {'posts' : posts})
-
