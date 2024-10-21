e2780d0b28358c62874665fcbc1e4053257cdb15
Tania Maldonado <taniamaldonado17@gmail.com>
2024-10-21 13:56:19 -0300

    borrando cositas innecesarias del form

--- blog/apps/posts/form.py
+++ blog/apps/posts/form.py
@@ -1,7 +1,3 @@
-'''from django import forms
-from .models import Comentario, CustomUser
-from django.contrib.auth.forms import UserCreationForm'''
-
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import User  # Asegúrate de usar el modelo personalizado
@@ -20,49 +16,6 @@ class ComentarioForm(forms.ModelForm):
             'rows': '1',
             'class': 'form-control'  
         })
-
-#######################################################################
-
-'''#Formulario de registro
-class RegistroForm(UserCreationForm):
-    username = forms.CharField(
-        max_length=200,
-        widget=forms.TextInput(
-            attrs={"class": "form-control", "placeholder": "Nombre de usuario"},
-        ),
-    )
-    email = forms.EmailField(
-        max_length=200,
-        help_text="Required",
-        widget=forms.EmailInput(
-            attrs={"class": "form-control", "placeholder": "name@example.com"}
-        ),
-    )
-    password1 = forms.CharField(
-        widget=forms.PasswordInput(attrs={"class": "form-control"}), required=True
-    )
-    password2 = forms.CharField(
-        widget=forms.PasswordInput(attrs={"class": "form-control"}), required=True
-    )
-  
-    icono = forms.ImageField(
-        label="Imagen de perfil",
-        required=False,
-        widget=forms.FileInput(attrs={"class": "form-control form-control-lg"}), 
-    )
-
-    class Meta:
-        model = User  
-        fields = [
-            "username",
-            "email",
-            "password1",
-            "password2",
-            "icono",
-        ]'''
-                
-
-#########################################################################
 
 
 #Formulario de registro
